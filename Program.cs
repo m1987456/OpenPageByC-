6387f9895a4a6e05d0b691678a32ee3bb8a51702
Mohammad Malla Othman <35405035+m1987456@users.noreply.github.com>
2018-03-08 21:44:17 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,11 +16,8 @@ namespace ConsoleApp1
             CookieContainer gaCookies = new CookieContainer();
             Uri target = new Uri("https://www.facebook.com/M8tartCool/posts/");
 
-            gaCookies.Add(new Cookie("sb", "zFaYWvAncQYxwpo2YHyvnhyy") { Domain = target.Host });
-            gaCookies.Add(new Cookie("fr", "0vW5WENEgC9XJzqix.AWXaK3WFXvdmzNDHNd1DmSHJJl0.BamFbM.c_.AAA.0.0.BamFbP.AWUIG5Cx") { Domain = target.Host });
-            gaCookies.Add(new Cookie("c_user", "100001599066593") { Domain = target.Host });
-            gaCookies.Add(new Cookie("xs", "7%3Al4KkXowz_Yf_mA%3A2%3A1519933135%3A18008%3A6878") { Domain = target.Host });
-            gaCookies.Add(new Cookie("pl", "n") { Domain = target.Host });
+            gaCookies.Add(new Cookie("sb", "exm") { Domain = target.Host });
+
 
             
             APIPorjectanalistic.ApiPorject m = new APIPorjectanalistic.ApiPorject(gaCookies);
